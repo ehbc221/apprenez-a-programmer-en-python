922d1127eb40f4fb0c50abc5d68f3f22c41fe9bd
ehbc221 <ehbc221@gmail.com>
2017-05-04 23:52:41 +0000

    first 7 chapters done

---
+++ autres/pas-a-pas-vers-la-modularite-2-2/test.py
@@ -0,0 +1,9 @@
+# -*-coding:Utf-8 -*
+
+from multipli import *
+
+# test de la fonction table
+table(3, 20)
+
+# Mettre en pause notre programme(ou bien os.system("pause") sur Windows)
+input("Appuyez sur ENTREE pour continuer...")
---
+++ autres/test_package.py
@@ -0,0 +1,4 @@
+# -*-coding:Utf-8 -*
+
+from package.fonctions import table # ou bien import package.fonctions
+table(5) # Appel de la fonction table
